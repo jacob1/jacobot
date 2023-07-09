face9a85cc73b1e286235935a36539fcdf0bd54f
jacob1 <jfu614@gmail.com>
2023-07-09 00:22:59 -0400

    seenmc: Track connection / disconnection too, and display it sometimes

--- mods/seenmc.py
+++ mods/seenmc.py
@@ -5,15 +5,27 @@ import config
 from common import *
 RegisterMod(__name__)
 
+botMessage = r"^:(?:(potato|mc|creative)relay!~mcrelay@user/jacob1/bot/potatorelay) PRIVMSG #powder-mc :"
 def Parse(raw, text):
-	match = re.match(r"^:(?:(potato|mc|creative)relay!~mcrelay@user/jacob1/bot/potatorelay) PRIVMSG #powder-mc :<([^>]+)\x0F> (.+)$", raw)
+	match = re.match(botMessage + r"<([^>]+)\x0F> (.+)$", raw)
+	connection = False
+	if not match:
+		match = re.match(botMessage + r"\x0314\[([^ ]+) ((?:dis)?connected)\]", raw)
+		connection = True
+
 	if match:
 		server = "unknown server"
 		if match.group(1) == "mc":
 			server = "survival"
 		elif match.group(1) == "creative":
 			server = "creative"
-		StoreData(__name__, match.group(2), {"server":server,"message":match.group(3),"time":time.time()})
+
+		username = match.group(2)
+		message = match.group(3)
+		if not connection:
+			StoreData(__name__, username, {"server":server,"message":message,"time":time.time()})
+		else:
+			StoreData(__name__, f"connection-{username}", {"server":server,"type":message,"time":time.time()})
 
 @command("seenmc", minArgs=1)
 def SeenMc(message):
@@ -22,8 +34,14 @@ def SeenMc(message):
 		raise ShowHelpException()
 	user = message.GetArg(0)
 	lastSeen = GetData(__name__, user)
-	if not lastSeen:
-		message.Reply("{0} has not been seen".format(user))
+	lastConnection = GetData(__name__, f"connection-{user}")
+	if not lastSeen and not lastConnection:
+		message.Reply("{0} has not been seen on Minecraft".format(user))
 	else:
-		timemsg = datetime.datetime.utcfromtimestamp(lastSeen["time"]).strftime("%a %b %d %Y at %I:%M:%S%p UTC")
-		message.Reply("{0} was last seen on {1} on {2}: {3}".format(user, lastSeen["server"], timemsg, lastSeen["message"]))
+		if lastSeen:
+			timemsg = datetime.datetime.utcfromtimestamp(lastSeen["time"]).strftime("%a %b %d %Y at %I:%M:%S%p UTC")
+			message.Reply("{0} was last seen on {1} on {2}: {3}".format(user, lastSeen["server"], timemsg, lastSeen["message"]))
+		if lastConnection and lastConnection["time"] - 600 > lastSeen["time"]:
+			timemsg = datetime.datetime.utcfromtimestamp(lastConnection["time"]).strftime("%a %b %d %Y at %I:%M:%S%p UTC")
+			connectionType = "disconnected from" if lastConnection["type"] == "disconnected" else "connected to"
+			message.Reply("{0} last {1} {2} on {3}".format(user, connectionType, lastSeen["server"], timemsg))
